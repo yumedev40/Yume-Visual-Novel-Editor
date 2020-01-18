086d01df2787964c713e90021c0a0f9c99617ea7
yumedev40 <59782287+yumedev40@users.noreply.github.com>
2020-01-18 11:06:35 -0800

    Added yume game controller singleton
    Deleted old placeholder var file that was unused

---
+++ Yume_VisualNovelEditor/AutoLoad_Scripts/yume_game_controller_singleton.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#warnings-disable
+
+var initial_scene : String = ""
+
+var directory_paths : Dictionary = {
+	"project_directory":null,
+	"game_data":null,
+	"story_data":null,
+	"game_scenes":null
+	}
+
+
+
